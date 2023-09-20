16520dd7ca0c6c2ed83b6f5b4d0d0a82b3a70cfb
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-09-20 10:26:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket tjänar person 1 i ditt företag?");
+            float X = float.Parse(Console.ReadLine());
+            Console.WriteLine(" Hur mycket tjänar person 2 i ditt företag?");
+            float Y = float.Parse(Console.ReadLine());
+            Console.WriteLine(" Hur mycket tjänar person 3 i ditt företag?");
+            float Z = float.Parse(Console.ReadLine());
+            float medel = (X + Y + Z) / 2;
+            Console.WriteLine("Medelönen för personalen är" + medel);
+        }
+    }
+}
